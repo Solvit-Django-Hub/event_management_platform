a85f89c853a2c3b60edcdfef410fcbc22f74d6ac
Semicolon-rw <Adol1329@users.noreply.github.com>
2026-09-11 16:06:36 +0200

    feat: implement authentication with JWT

--- event_management_platform/settings.py
+++ event_management_platform/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "django_filters",
+    "rest_framework_simplejwt.token_blacklist",
 
     # Local apps
     "apps.accounts",
@@ -51,6 +52,30 @@ INSTALLED_APPS = [
     "apps.bookings",
     "apps.payments",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
+
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ),
+
+    "DEFAULT_PAGINATION_CLASS": (
+        "rest_framework.pagination.PageNumberPagination"
+    ),
+
+    "PAGE_SIZE": 10,
+}
+
+AUTH_USER_MODEL = "accounts.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
